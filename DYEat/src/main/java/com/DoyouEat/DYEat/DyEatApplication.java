4d28d7aac208cc4906d57a5b409c2431958d4f30
backgom1 <dmstjd0214@naver.com>
2022-11-12 16:35:55 +0900

    DoyouEat Project 생성

---
+++ DYEat/src/main/java/com/DoyouEat/DYEat/DyEatApplication.java
@@ -0,0 +1,13 @@
+package com.DoyouEat.DYEat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DyEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DyEatApplication.class, args);
+	}
+
+}
---
+++ DYEat/src/test/java/com/DoyouEat/DYEat/DyEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DoyouEat.DYEat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DyEatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
