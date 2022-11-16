354d12235a440fc4c2d1c12610f69e675af424cb
backgom1 <dmstjd0214@naver.com>
2022-11-16 23:17:09 +0900

    pay,orderDetail,main 의 Controller 및 html 제작
    추후 개발사항
    orderList, signup ,mybag,mypage Controller 제작

---
+++ DYEat/src/main/java/com/DoyouEat/DYEat/controller/order/orderController.java
@@ -0,0 +1,18 @@
+package com.DoyouEat.DYEat.controller.order;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/order")
+public class orderController {
+
+
+    @GetMapping
+    public String orderDetail(){
+        return "views/order/orderDetail";
+    }
+
+
+}
---
+++ DYEat/src/main/java/com/DoyouEat/DYEat/controller/signup/signupController.java
@@ -0,0 +1,7 @@
+package com.DoyouEat.DYEat.controller.signup;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class signupController {
+}
